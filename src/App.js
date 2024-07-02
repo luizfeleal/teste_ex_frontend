370da53d21422b02c4fa77c4e74cd4ef8a7d3638
Luiz Felipe <felipelearaujo@gmail.com>
2024-07-01 23:54:29 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <h1>Luiz Felipe</h1>
+      
+    </div>
+  );
+}
+
+export default App;
